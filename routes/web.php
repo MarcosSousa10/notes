61daa9e81ee50256dda398b026a130fae7fcd979
Marcos <marcospegodesousa10@gmail.com>
2024-10-03 13:24:54 -0300

    passando valores via url

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('main');
 });
+Route::get('/about', function () {
+    echo 'About us';
+});
+Route::get('/Main/{value}',[MainController::class, 'index']);
