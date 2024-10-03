7d9c7d7190f90b654cefab350e9433e6b3323e5b
Marcos <marcospegodesousa10@gmail.com>
2024-10-03 15:18:16 -0300

    validação de login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,8 +15,15 @@ class AuthController extends Controller
 
         $request->validate(
             [
-                'text_username' => 'required',
-                'text_password' => 'required'
+                'text_username' => 'required|email',
+                'text_password' => 'required|min:6|max:16'
+            ],
+            [
+                'text_username.required' => 'O username e obrigatorio',
+                'text_username.email' => 'Username deve ser um email valido',
+                'text_password.required' => 'O password e obrigatorio',
+                'text_password.min' => 'O password deve ter pelo menos :min caracteres',
+                'text_password.max' => 'O password deve ter no maximo :max caracteres'
             ]
 
         );
