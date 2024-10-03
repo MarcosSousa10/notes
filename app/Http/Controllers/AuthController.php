71470709442b5907d6a5478544545bfe38064f46
Marcos <marcospegodesousa10@gmail.com>
2024-10-03 14:53:01 -0300

    tela login validação campos null

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,10 +6,27 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login(){
-        echo "login";
+    public function login()
+    {
+        return view('login');
     }
-    public function logout(){
+    public function loginSubmit(Request $request)
+    {
+
+        $request->validate(
+            [
+                'text_username' => 'required',
+                'text_password' => 'required'
+            ]
+
+        );
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+        echo "<br>";
+        dd($request);
+    }
+    public function logout()
+    {
         echo "logout";
     }
 }
